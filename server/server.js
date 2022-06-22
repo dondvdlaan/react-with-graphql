1c69dd4ddbdf520b9cf3288fb4268824477d4288
dondvdlaan <dondvdlaan@gmail.com>
2022-06-22 12:40:08 +0200

    Initial upload

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// Constants andvariables
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const server = express();
+const port = 4000;
+const cors = require('cors');
+
+//This is CORS-enabled for all origins
+server.use(cors());
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
+
+// Starting server
+server.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+server.listen(
+    port,
+    err => console.log(err || `GraphQL API server at http://localhost:${port}/graphql`));
